7b2fd5c2bd32bb492c3fa6eca70c5ff373b73056
unknown <dxvu2205@gmail.com>
2023-09-05 19:30:16 +0700

    setup fe

---
+++ asp_DangXuanVu/Controllers/TrangChuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp_DangXuanVu.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        // GET: TrangChu/index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
