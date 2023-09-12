4611a2ef04970166183eb14e7259bd75cc5ac11a
unknown <dxvu2205@gmail.com>
2023-09-12 18:58:05 +0700

    add link

--- packages/Modernizr.2.8.3/Tools/common.ps1
+++ packages/Modernizr.2.8.3/Tools/common.ps1
@@ -1,75 +1,37 @@
-function AddOrUpdate-Reference($scriptsFolderProjectItem, $fileNamePattern, $newFileName) {
-    try {
-        $referencesFileProjectItem = $scriptsFolderProjectItem.ProjectItems.Item("_references.js")
-    }
-    catch {
-        # _references.js file not found
-        return
-    }
-
-    if ($referencesFileProjectItem -eq $null) {
-        # _references.js file not found
-        return
-    }
-
-    $referencesFilePath = $referencesFileProjectItem.FileNames(1)
-    $referencesTempFilePath = Join-Path $env:TEMP "_references.tmp.js"
-
-    if ((Select-String $referencesFilePath -pattern $fileNamePattern).Length -eq 0) {
-        # File has no existing matching reference line
-        # Add the full reference line to the beginning of the file
-        "/// <reference path=""$newFileName"" />" | Add-Content $referencesTempFilePath -Encoding UTF8
-         Get-Content $referencesFilePath | Add-Content $referencesTempFilePath
-    }
-    else {
-        # Loop through file and replace old file name with new file name
-        Get-Content $referencesFilePath | ForEach-Object { $_ -replace $fileNamePattern, $newFileName } > $referencesTempFilePath
-    }
-
-    # Copy over the new _references.js file
-    Copy-Item $referencesTempFilePath $referencesFilePath -Force
-    Remove-Item $referencesTempFilePath -Force
-}
-
-function Remove-Reference($scriptsFolderProjectItem, $fileNamePattern) {
-    try {
-        $referencesFileProjectItem = $scriptsFolderProjectItem.ProjectItems.Item("_references.js")
-    }
-    catch {
-        # _references.js file not found
-        return
-    }
-
-    if ($referencesFileProjectItem -eq $null) {
-        return
-    }
-
-    $referencesFilePath = $referencesFileProjectItem.FileNames(1)
-    $referencesTempFilePath = Join-Path $env:TEMP "_references.tmp.js"
-
-    if ((Select-String $referencesFilePath -pattern $fileNamePattern).Length -eq 1) {
-        # Delete the line referencing the file
-        Get-Content $referencesFilePath | ForEach-Object { if (-not ($_ -match $fileNamePattern)) { $_ } } > $referencesTempFilePath
-
-        # Copy over the new _references.js file
-        Copy-Item $referencesTempFilePath $referencesFilePath -Force
-        Remove-Item $referencesTempFilePath -Force
-    }
-}
-
-# Extract the version number from the file in the package's content\scripts folder
-$packageScriptsFolder = Join-Path $installPath Content\Scripts
-$modernizrFileName = Join-Path $packageScriptsFolder "modernizr-*.js" | Get-ChildItem -Exclude "*.min.js","*-vsdoc.js" | Split-Path -Leaf
-$modernizrFileNameRegEx = "modernizr-((?:\d+\.)?(?:\d+\.)?(?:\d+\.)?(?:\d+)).js"
-$modernizrFileName -match $modernizrFileNameRegEx
-$ver = $matches[1]
-
-# Get the project item for the scripts folder
-try {
-    $scriptsFolderProjectItem = $project.ProjectItems.Item("Scripts")
-    $projectScriptsFolderPath = $scriptsFolderProjectItem.FileNames(1)
-}
-catch {
-    # No Scripts folder
-    Write-Host "No scripts folder found"
-}
+80C75CF45132CFDC45A3E97722AF99AE4C30A9768ABC2937A1713DE" restart="required">
+      <driver inf="prnms011.inf" ranking="normal">
+        <assemblyIdentity name="dual_prnms011.inf" version="10.0.19041.1" processorArchitecture="amd64" language="neutral" publicKeyToken="31bf3856ad364e35" buildType="release" versionScope="nonSxS" type="dualModeDriver" />
+      </driver>
+    </update>
+    <update name="ADE0F63A363664C68161FECFF7E1D17E4BF32469E30A712347E457E01EC7EDDB" restart="required">
+      <driver inf="prnms012.inf" ranking="normal">
+        <assemblyIdentity name="dual_prnms012.inf" version="10.0.19041.1" processorArchitecture="amd64" language="neutral" publicKeyToken="31bf3856ad364e35" buildType="release" versionScope="nonSxS" type="dualModeDriver" />
+      </driver>
+    </update>
+    <update name="7D9D145CB8845B6D9006268A5EE52908F04079F22F2125EA2BA97FDFB1612E8B" restart="required">
+      <driver inf="prnms013.inf" ranking="normal">
+        <assemblyIdentity name="dual_prnms013.inf" version="10.0.19041.1" processorArchitecture="amd64" language="neutral" publicKeyToken="31bf3856ad364e35" buildType="release" versionScope="nonSxS" type="dualModeDriver" />
+      </driver>
+    </update>
+    <update name="F4C89C2C09F39834992D1FBEAF14B68B6D45C92687CEAE8FEFBB3750CD6B3C65" restart="required">
+      <driver inf="prnms014.inf" ranking="normal">
+        <assemblyIdentity name="dual_prnms014.inf" version="10.0.19041.1806" processorArchitecture="amd64" language="neutral" publicKeyToken="31bf3856ad364e35" buildType="release" versionScope="nonSxS" type="dualModeDriver" />
+      </driver>
+    </update>
+    <update name="A19173C2959538F8F40916D3BFFEF1F78B38498A28849E5B58912E0CE634157F" restart="required">
+      <driver inf="sti.inf" ranking="normal">
+        <assemblyIdentity name="dual_sti.inf" version="10.0.19041.1806" processorArchitecture="amd64" language="neutral" publicKeyToken="31bf3856ad364e35" buildType="release" versionScope="nonSxS" type="dualModeDriver" />
+      </driver>
+    </update>
+    <update name="8BB71BECE9C4031E1242D01817BFDA883A1C4605E918C942DB7D465DCCA5ED9C" restart="required">
+      <driver inf="wsdprint.inf" ranking="normal">
+        <assemblyIdentity name="dual_wsdprint.inf" version="10.0.19041.1806" processorArchitecture="amd64" language="neutral" publicKeyToken="31bf3856ad364e35" buildType="release" versionScope="nonSxS" type="dualModeDriver" />
+      </driver>
+    </update>
+    <update name="FDC9A47B30AEEFF17ADB465F61545C6028FCBDA9B03114095699397CB6F3BA45" restart="required">
+      <driver inf="wsdscdrv.inf" ranking="normal">
+        <assemblyIdentity name="dual_wsdscdrv.inf" version="10.0.19041.1806" processorArchitecture="amd64" language="neutral" publicKeyToken="31bf3856ad364e35" buildType="release" versionScope="nonSxS" type="dualModeDriver" />
+      </driver>
+    </update>
+    <update name="C0F05A27C6A13EA299304E6FC82BCBFB37A0B88BC5E02207A8A89B561D18175E" restart="required">
+      <driver inf="
